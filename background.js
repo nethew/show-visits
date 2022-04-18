6368310c5c8603765158cc7183c1e7fceee421e1
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-04-18 19:42:35 +0200

    feat: show list of visits chore: rename addon

--- background.js
+++ background.js
@@ -1,44 +1,16 @@
 
-async function listenForClicks(url){
-    //const host = new URL(url)?.origin;
-    //if(typeof host === 'string'){
-        const results = await browser.history.getVisits({ url: url});
-        if(results.length > 1) {
-            return new Date(results[1].visitTime);
-        }
-    //}
-    return new Date();
-}
+async function onMessage(data, sender) {
+    if (data.getVisits) {
+        const atabs = await browser.tabs.query({currentWindow: true, active: true});
+        if(atabs.length > 0){
+            //console.log(atabs[0].url);
+            const visits = (await browser.history.getVisits({ url: atabs[0].url})).map( v => v.visitTime);
 
-function getwithZero(number){
-    return `${number}`.padStart(2, '0');
-}
-
-const filter = {
-  properties: ["url"]
-}
-
-
-browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tabInfo) => {
-
-    if(changeInfo.url) {
-        console.log(changeInfo.url);
-
-        const lastSeen = await listenForClicks(changeInfo.url);
-        if(lastSeen) {
-            const year = lastSeen.getFullYear();
-            const month = getwithZero(lastSeen.getMonth() + 1);
-            const date = getwithZero(lastSeen.getDate());
-            const hour = getwithZero(lastSeen.getHours());
-            const minutes = getwithZero(lastSeen.getMinutes());
-            const lastVisitedtitle  = `${year}-${month}-${date} ${hour}:${minutes}`;
-            console.log(lastVisitedtitle);
-
-            browser.pageAction.setTitle({
-                tabId: tabId,
-                title:lastVisitedtitle
-            });
+            return { 'url': atabs[0].url, 'visits': visits };
         }
     }
+    return [];
+}
 
-}, filter);
+browser.runtime.onMessage.addListener(onMessage);
+
---
+++ popup.js
@@ -0,0 +1,78 @@
+
+function getwithZero(number){
+    return `${number}`.padStart(2, '0');
+}
+
+function visitTime2String(time) {
+
+    const visitTime = new Date(time);
+
+    const year = visitTime.getFullYear();
+    const month = getwithZero(visitTime.getMonth() + 1);
+    const date = getwithZero(visitTime.getDate());
+    const hour = getwithZero(visitTime.getHours());
+    const minutes = getwithZero(visitTime.getMinutes());
+    const tmp = `${year}-${month}-${date} ${hour}:${minutes}`;
+
+    return tmp;
+
+}
+
+
+function visitTime2LinkString(time) {
+
+    const visitTime = new Date(time);
+
+    const year = visitTime.getFullYear();
+    const month = getwithZero(visitTime.getMonth() + 1);
+    const date = getwithZero(visitTime.getDate());
+    const hour = getwithZero(visitTime.getHours());
+    const minutes = getwithZero(visitTime.getMinutes());
+    const tmp = `${year}${month}${date}${hour}${minutes}00`;
+    return tmp;
+
+}
+
+async function init() {
+
+    const tbl = document.getElementById('visitlist');
+
+    const message = {'getVisits': true }
+    let data = await browser.runtime.sendMessage(message);
+
+    let idCounter = 1
+
+    data.visits.forEach((visit) => {
+        const tr = tbl.insertRow()
+        const span = document.createElement('a')
+        span.textContent = visitTime2String(visit);
+        span.target = '_blank';
+        span.href = 'https://web.archive.org/web/' + visitTime2LinkString(visit)+ '*/' + data.url;
+        var td = tr.insertCell()
+
+        switch(idCounter){
+            case 1:
+                td.textContent = idCounter + '. Current: ';
+                break;
+            case 2:
+                td.textContent = idCounter + '. Before';
+                break;
+            default:
+                td.textContent = idCounter + '. ';
+                break;
+        }
+        td.appendChild(span)
+        idCounter++
+    });
+}
+
+init();
+
+// open options and close popup
+document.getElementById("about").addEventListener('click', function(evt) {
+    browser.tabs.create({
+        url:"https://addons.mozilla.org/firefox/addon/show-visits"
+        ,active: true
+	});
+    window.close();
+});
